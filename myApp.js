338f967d85fa3120e5e9b763f0d662ac5892a1e8
Preetham Ananthkumar <preetham.mervin@gmail.com>
2024-07-16 12:24:20 +0000

    Refactored to Replit

--- myApp.js
+++ myApp.js
@@ -1,59 +1,15 @@
-const express = require('express');
-var helmet = require('helmet');
+const express = require("express");
+const helmet = require("helmet");
 const app = express();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 module.exports = app;
-const api = require('./server.js');
+const api = require("./server.js");
 app.use(helmet());
-app.use(express.static('public'));
-app.disable('strict-transport-security');
-app.use('/_api', api);
+app.use(express.static("public"));
+app.disable("strict-transport-security");
+app.use("/_api", api);
 app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+  response.sendFile(__dirname + "/views/index.html");
 });
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
